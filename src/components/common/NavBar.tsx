c6ac2dc104d0626c2e99af354276d89514afb120
Hemit Patel <hemitvpatel@gmail.com>
2025-01-05 16:34:38 -0500

    fixed where nav attr should be

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -12,8 +12,8 @@ const NavBar = () => {
   };
 
   return (
-    <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav className="bg-white">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
@@ -42,7 +42,7 @@ const NavBar = () => {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" className="text-sm font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
-      </nav>
+      </div>
       
       {mobileMenuOpen && (
         <div className="lg:hidden" role="dialog" aria-modal="true">
@@ -81,7 +81,7 @@ const NavBar = () => {
           </div>
         </div>
       )}
-    </header>
+    </nav>
   );
 };
 
