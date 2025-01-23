98a5cf8c79da0c4cc5215141bf06026cdc7d0cb8
Hemit Patel <hemitvpatel@gmail.com>
2025-01-23 17:35:46 -0500

    added about component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/landing-page/About";
 import Hero from "@/components/landing-page/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
