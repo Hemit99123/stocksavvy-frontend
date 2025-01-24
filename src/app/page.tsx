4f2c4ee01f0a763d9e0d36a3ff36d7bc401931b0
Hemit Patel <hemitvpatel@gmail.com>
2025-01-24 17:56:37 -0500

    added opacity transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion"; // Import motion from framer-motion
 import NavBar from "@/components/common/NavBar";
 import About from "@/components/landing-page/About";
 import Hero from "@/components/landing-page/Hero";
@@ -9,28 +10,29 @@ import Loading from "@/components/common/Loading";
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
-
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1500); // Show loading for 500ms, afterwards remove the loading state
+    }, 1500); // Show loading for 1500ms, afterwards remove the loading state
 
     return () => clearTimeout(timer); // Clean up timer on component unmount
   }, []);
-  
 
   if (isLoading) {
     return <Loading />;
   }
 
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}      // Start with opacity 0
+      animate={{ opacity: 1 }}      // Animate to opacity 1
+      transition={{ duration: 1.5 }}  // Duration of the opacity transition
+    >
       <div className="bg-gradient-to-br from-yellow-200 via-lime-400 to-green-500 rounded-b-3xl">
         <NavBar />
         <Hero />
       </div>
       <About />
-    </div>
+    </motion.div>
   );
 }
