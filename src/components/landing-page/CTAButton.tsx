1b1dfaaa4381b4897e31c0e217d41a6b1e7a97e3
Hemit Patel <hemitvpatel@gmail.com>
2025-01-23 19:20:28 -0500

    fixed btn styling

--- src/components/landing-page/CTAButton.tsx
+++ src/components/landing-page/CTAButton.tsx
@@ -3,13 +3,14 @@ import React, { FC } from 'react';
 interface CTAButtonProps {
   link: string;
   text: string;
+  isTransparent: boolean;
 }
 
-const CTAButton: FC<CTAButtonProps> = ({ link, text }) => {
+const CTAButton: FC<CTAButtonProps> = ({ link, text, isTransparent }) => {
   return (
     <a
       href={link}
-      className="bg-black rounded-full text-white px-4 py-2 text-sm"
+      className={`font-bold rounded-full text-sm px-4 py-2 transition-opacity duration-300 ${isTransparent ? 'bg-transparent border border-black text-black hover:opacity-80' : 'bg-black text-white hover:opacity-80'}`}
     >
       <p>{text}</p>
     </a>
--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -5,24 +5,28 @@ const Hero = () => {
   return (
     <section>
         <div className="grid max-w-screen-xl mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12 lg:pt-20">
-        <div className="place-self-center lg:col-span-7">
-            <h3 className="md:text-5xl xl:text-8xl font-bold">Finance is key.</h3>
-            <p className="mb-6 lg:mb-8 lg:text-xl pt-4">
-                Stock Savvy is a financial literacy initiative that helps young and aspiring high school students. 
-            </p>
-            <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4 mb-10">
-            <CTAButton 
-                link="/"
-                text="Get Started"
-            />
-            <CTAButton 
-                link="https://www.instagram.com/stock.savvy_/"
-                text="Instagram"
-            />
+            <div className="place-self-center lg:col-span-7 px-4 sm:px-6 md:px-8">
+                <h3 className="md:text-5xl xl:text-8xl text-3xl font-bold leading-tight">
+                    Finance is key.
+                </h3>
+                <p className="mb-6 lg:mb-8 lg:text-xl pt-4 text-base">
+                    Stock Savvy is a financial literacy initiative that helps young and aspiring high school students. 
+                </p>
+                <div className="flex space-x-2 mb-44">
+                    <CTAButton 
+                        isTransparent={false}
+                        link="/"
+                        text="Get Started"
+                    />
+                    <CTAButton 
+                        isTransparent={true}
+                        link="https://www.instagram.com/stock.savvy_/"
+                        text="Instagram"
+                    />
+                </div>
             </div>
         </div>
-        </div>
-  </section>
+    </section>
   )
 }
 
