5fbfdcde5c7f9defdd672376eca291d60204e173
Hemit Patel <hemitvpatel@gmail.com>
2025-01-24 09:36:05 -0500

    removed bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/landing-page/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[#e2e7f1]">
+    <div>
       <div className="bg-gradient-to-br from-yellow-200 via-lime-400 to-green-500 rounded-b-3xl">
         <NavBar />
         <Hero />
