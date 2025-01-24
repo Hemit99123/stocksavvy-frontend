1d00e454f37043c95fc0fb8179a22f186e5a0007
Hemit Patel <hemitvpatel@gmail.com>
2025-01-24 16:19:20 -0500

    added date + made things responsive

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -1,15 +1,44 @@
 'use client'
- 
+
 import { useSearchParams } from 'next/navigation'
- 
+import { workshopItems } from '@/data/workshop'
+
 export default function SearchBar() {
   const searchParams = useSearchParams()
- 
   const slug = searchParams.get('slug')
 
+  // Filter workshopItems based on the slug
+  const filteredItems = slug ? workshopItems.filter(item => item.slug === slug) : workshopItems
+
+  const item = filteredItems[0]
+
   return (
-    <div>
-        slug: {slug}
-    </div>
+    <>
+        {slug ? (
+          <div className="flex flex-col lg:flex-row h-auto lg:h-screen">
+            <div className="w-full lg:w-1/2 h-full relative">
+                <img 
+                    src={item.img}
+                    className="rounded-r-3xl w-full h-full object-cover hidden lg:block"
+                />
+                <div className="absolute bottom-4 left-4 bg-lime-200 px-11 py-2 rounded-xl font-medium">
+                    {item.date}
+                </div>
+            </div>
+            <div className="lg:mt-0 lg:ml-8 w-full lg:w-1/2">
+                <h1 className="mt-7 text-4xl lg:text-6xl font-custom">
+                    {item.title}
+                </h1>
+                <div className="mt-5 max-w-full lg:max-w-xl">
+                    <p className="text-lg break-words">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra nec enim eget hendrerit. Fusce nisi nisi, ullamcorper quis ante non, malesuada blandit odio. Vestibulum a felis ornare, venenatis enim quis, suscipit quam. Maecenas fringilla congue est, ac pellentesque tortor vehicula sit amet. Nam rutrum, mauris in auctor convallis, lacus mi malesuada neque, et congue turpis tortor ac odio. Praesent euismod lobortis quam id vestibulum. Nullam lobortis tellus vel malesuada interdum.                    
+                    </p>
+                </div>
+            </div>
+          </div>
+        ) : (
+          <div>No slug provided.</div>
+        )}
+    </>
   )
 }
--- src/data/workshop.ts
+++ src/data/workshop.ts
@@ -4,6 +4,7 @@
     title: string;
     slug: string;
     fontColor: "white" | "black"; // Union type for fontColor
+    date: string;
   }
   
 
@@ -13,30 +14,35 @@ export const workshopItems: WorkshopItem[] = [
       img: 'https://educationontario.com/app/uploads/2022/04/Photos.jpg',
       title: 'MCMASTER',
       slug: "mcmaster",
-      fontColor: 'white'
+      fontColor: 'white',
+      date: "November 28 2024"
     },
     {
       img: 'https://www.mbaresearch.org/wp-content/uploads/2024/10/deca-checklist-cover.png',
       title: 'BRAMELA DECA',
       slug: "bramela_deca",
-      fontColor: 'white'
+      fontColor: 'white',
+      date: "December 7 2024"
     },
     {
       img: 'https://www.educationusacanada.ca/wp-content/uploads/2022/01/sat.jpeg',
       title: 'DAILYSAT',
       slug: "dailysat",
-      fontColor: 'black'
+      fontColor: 'black',
+      date: "December 21 2024"
     },
     {
       img: 'https://images.stockcake.com/public/1/7/4/174c0bce-6c5d-46f4-a7a0-1e93c2cd00e4_large/stock-exchange-interior-stockcake.jpg',
       title: 'STOCKS',
       slug: "stocks",
-      fontColor: 'white'
+      fontColor: 'white',
+      date: "December 14 2024"
     },
     {
       img: 'https://ocdn.eu/pulscms-transforms/1/FtwktkpTURBXy82M2FlMGVlNjc1YzM0NTBkNWVmNjA2NTIyYzIyM2Y1Zi5qcGeSlQMAL80F3M0DS5MFzQMWzQGu',
       title: 'BUDGETING',
       slug: "budgeting",
-      fontColor: 'black'
+      fontColor: 'black',
+      date: "December 2 2024"
     }
   ];
