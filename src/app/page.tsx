09e714b6fe2fd877ff498d6eba082ba44ae39795
Hemit Patel <hemitvpatel@gmail.com>
2025-01-24 16:55:36 -0500

    added basic loading stuff + added loading func to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client"
+
+import { useState, useEffect } from "react";
 import NavBar from "@/components/common/NavBar";
 import About from "@/components/landing-page/About";
 import Hero from "@/components/landing-page/Hero";
+import Loading from "@/components/common/Loading";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // Show loading for 500ms
+
+    return () => clearTimeout(timer); // Clean up timer on component unmount
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <div className="bg-gradient-to-br from-yellow-200 via-lime-400 to-green-500 rounded-b-3xl">
--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,1 +1,15 @@
+import React from 'react'
 
+const Loading = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-yellow-200 via-lime-400 to-green-500">
+        <img 
+            src="/images/logo.png"
+            className="h-80 w-80"
+        />
+        <h1 className="text-3xl">StockSavvy</h1>
+    </div>
+  )
+}
+
+export default Loading
