d6ea83e02ee0e0966f21f56e061a465338cbbe65
Hemit Patel <hemitvpatel@gmail.com>
2025-01-24 14:25:49 -0500

    changed size handling

--- src/components/landing-page/common/AboutItem.tsx
+++ src/components/landing-page/common/AboutItem.tsx
@@ -8,11 +8,11 @@ interface AboutItemProps {
 const AboutItem: React.FC<AboutItemProps> = ({icon, title, desc}) => {
   return (
     <div className="text-start">
-        <div className="text-xl text-green-900">
+        <div className="text-xl lg:text-3xl text-green-900">
             {icon}
         </div>
         <h3 className="text-lg font-bold">{title}</h3>
-        <div className="max-w-4xl">
+        <div className="max-w-4xl text-sm text-gray-600">
             <p>{desc}</p>
         </div>
     </div>
