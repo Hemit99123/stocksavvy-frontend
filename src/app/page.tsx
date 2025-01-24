6ee9da005d4ad452c77e918fd1d254e70ef839c4
Hemit Patel <hemitvpatel@gmail.com>
2025-01-23 19:40:02 -0500

    added bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import NavBar from "@/components/common/NavBar";
+import About from "@/components/landing-page/About";
 import Hero from "@/components/landing-page/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#e2e7f1]">
       <div className="bg-gradient-to-br from-yellow-200 via-lime-400 to-green-500 rounded-b-3xl">
         <NavBar />
         <Hero />
       </div>
+      <About />
     </div>
   );
 }
