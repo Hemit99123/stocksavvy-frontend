31111d45b0fa3f0edda91dc08a00c2daeae7726a
Hemit Patel <hemitvpatel@gmail.com>
2025-01-26 13:01:27 -0500

    moved data to /data folder

--- src/components/landing-page/Testimonial.tsx
+++ src/components/landing-page/Testimonial.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
 import Header from './common/Header';
 import TestimonialCard from './common/TestimonialCard';
-import { TestimonialCardProps } from '@/types/testimonial';
+import {testimonials} from '@/data/testimonial'
 
 const Testimonial = () => {
-  const testimonials: TestimonialCardProps[] = [
-    {
-      color: 'yellow',
-      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
-      name: 'Azure F.',
-      date: 'May 2024',
-    },
-    {
-      color: 'cyan',
-      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
-      name: 'Azure F.',
-      date: 'May 2024',
-    },
-    {
-      color: 'green',
-      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
-      name: 'Azure F.',
-      date: 'May 2024',
-    },
-  ];
 
   return (
     <div className="mt-16 text-center">
---
+++ src/data/testimonial.tsx
@@ -0,0 +1,22 @@
+import { TestimonialCardProps } from "@/types/testimonial";
+
+export const testimonials: TestimonialCardProps[] = [
+    {
+      color: 'yellow',
+      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
+      name: 'Azure F.',
+      date: 'May 2024',
+    },
+    {
+      color: 'cyan',
+      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
+      name: 'Azure F.',
+      date: 'May 2024',
+    },
+    {
+      color: 'green',
+      text: 'The interface is easy to use and intuitive. All the information provided is very clear. As my first credit card experience, I 100% recommend them!',
+      name: 'Azure F.',
+      date: 'May 2024',
+    },
+];
