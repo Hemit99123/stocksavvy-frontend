2fcf01e74606efcd713cf86cbcce8bec545ba5d6
Hemit Patel <hemitvpatel@gmail.com>
2025-01-26 12:20:09 -0500

    changed text sizing

--- src/components/landing-page/common/Header.tsx
+++ src/components/landing-page/common/Header.tsx
@@ -7,16 +7,16 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({smalltext, title}) => {
   return (
     <div className="text-center">
-    <div className="flex items-center justify-center space-x-4 mb-4">
-      <div className="h-px w-12 bg-gradient-to-r from-transparent via-gray-500 to-transparent" />
-      <h2 className="text-sm lg:text-lg font-medium text-green-600">
-        {smalltext}
-      </h2>
-      <div className="h-px w-12 bg-gradient-to-r from-transparent via-gray-500 to-transparent" />
-    </div>
-    <h3 className="text-2xl lg:text-4xl font-semibold">
-        {title}
-    </h3>
+      <div className="flex items-center justify-center space-x-4 mb-4">
+        <div className="h-px w-12 bg-gradient-to-r from-transparent via-gray-500 to-transparent" />
+        <h2 className="text-sm lg:text-lg font-medium text-green-600">
+          {smalltext}
+        </h2>
+        <div className="h-px w-12 bg-gradient-to-r from-transparent via-gray-500 to-transparent" />
+      </div>
+      <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
+          {title}
+      </h3>
     </div>
   )
 }
