951d2f472519ee877ab70623cfe0e9a38e47d061
Hemit Patel <hemitvpatel@gmail.com>
2025-01-26 00:19:26 -0500

    changed cookie name

--- src/lib/loading.ts
+++ src/lib/loading.ts
@@ -5,7 +5,7 @@ import { cookies } from 'next/headers';
 export const setLoadingState = async (value: string) => {
   const cookieStore = cookies();
 
-  (await cookieStore).set('number_visited', value, {
+  (await cookieStore).set('loading_state', value, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // Only for production
     path: '/', // Cookie will be available on the entire site
@@ -16,7 +16,7 @@ export const getLoadingState = async () => {
   const cookieStore = cookies();
   
   // Retrieve the cookie value
-  const loadingState = (await cookieStore).get('number_visited');
+  const loadingState = (await cookieStore).get('loading_state');
 
   // Return a message or the value of the cookie
   if (loadingState) {
