7266c56bd5b7ee5add345a0caf818721e07e2538
Hemit Patel <hemitvpatel@gmail.com>
2025-01-26 12:18:11 -0500

    added basic parts

--- src/components/landing-page/Testimonial.tsx
+++ src/components/landing-page/Testimonial.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './common/Header'
 
 const Testimonial = () => {
   return (
-    <div>
-Testimonial
+    <div className="mt-16 text-center">
+        <Header 
+            smalltext="Our members"
+            title='Hear from our members'
+        />
+        <p className="mt-6">Real stories of what it means to learn with StockSavvy.</p>
     </div>
   )
 }
