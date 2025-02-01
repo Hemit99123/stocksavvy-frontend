762af2ca9a4f83fb076e48d68fca9db2b28d7842
Hemit Patel <hemitvpatel@gmail.com>
2025-01-31 20:20:03 -0500

    made text slightly lighter

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -4,8 +4,8 @@ import MultipleSpacer from "../common/MultipleSpacer";
 
 const Hero = () => {
   return (
-    <div className="lg:pt-16 lg:ml-32 text-center lg:text-start mb-8">
-      <h3 className="md:text-6xl xl:text-9xl text-4xl font-black leading-tight">
+    <div className="lg:pt-16 lg:ml-32 text-center lg:text-start mb-8 text-gray-900">
+      <h3 className="md:text-6xl xl:text-9xl text-4xl font-black leading-tight ">
         Finance is key.
       </h3>
       <p className="mb-6 lg:mb-8 text-base md:text-xl lg:text-2xl pt-4">
