e9e40ab7e791bf79c863beb82f16041ca4ab698a
Hemit Patel <hemitvpatel@gmail.com>
2025-01-31 20:26:34 -0500

    update coloring

--- src/components/landing-page/common/MissionItem.tsx
+++ src/components/landing-page/common/MissionItem.tsx
@@ -10,7 +10,7 @@ interface MissionItemProps {
 const MissionItem: React.FC<MissionItemProps> = ({ color, title, text, isLifted }) => {
   return (
     <div 
-      className={`${color === "green" ? "bg-green-400" : "bg-yellow-400"} 
+      className={`${color === "green" ? "bg-green-500 text-white" : "bg-yellow-400"} 
                   rounded-lg p-6 
                   ${isLifted ? 'lg:transform lg:translate-y-[-15px]' : 'lg:transform lg:translate-y-[15px]'}`}
     >
