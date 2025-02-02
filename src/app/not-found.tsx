ebc20eb0c766f8bed11b967e42681f813183c430
Hemit Patel <hemitvpatel@gmail.com>
2025-02-02 16:35:40 -0500

    fixing eslint error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
@@ -15,12 +16,12 @@ const NotFound = () => {
             Sorry, we can&apos;t find that page. You&apos;ll find lots to
             explore on the home page.{" "}
           </p>
-          <a
+          <Link
             href="/"
             className="inline-flex text-white bg-black hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
           >
             Back to Homepage
-          </a>
+          </Link>
         </div>
       </div>
     </div>
