43cb87e0a938b7be18eed34e39c0a84d753f19dc
Hemit Patel <hemitvpatel@gmail.com>
2025-02-02 14:15:11 -0500

    formatted the code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -54,43 +54,41 @@ const Auth = () => {
   return (
     <div className="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg">
       {/* Google Login */}
-        <div className="w-full mb-6">
-          <GoogleLogin
-            onSuccess={(response) => {
-              handleLoginGoogleSSO(response.credential); // This is your id token
-            }}
-            onError={() => {
-              alert("An error has occurred!");
-            }}
-            width="100%" // Make the Google Login button full width
-          />
-        </div>
+      <div className="w-full mb-6">
+        <GoogleLogin
+          onSuccess={(response) => {
+            handleLoginGoogleSSO(response.credential); // This is your id token
+          }}
+          onError={() => {
+            alert("An error has occurred!");
+          }}
+          width="100%" // Make the Google Login button full width
+        />
+      </div>
 
       <hr className="my-6 border-gray-300" />
 
       {/* Email Login */}
-
       {!isOTPGenerated && (
         <>
-              <h2 className="text-xl font-semibold mb-4">Login with Email</h2>
-      <div className="mb-4">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
-        />
-        <button
-          onClick={handleGenerateOTP}
-          className="w-full px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-        >
-          Generate OTP
-        </button>
-      </div>
+          <h2 className="text-xl font-semibold mb-4">Login with Email</h2>
+          <div className="mb-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+            />
+            <button
+              onClick={handleGenerateOTP}
+              className="w-full px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+            >
+              Generate OTP
+            </button>
+          </div>
         </>
       )}
-
 
       {isOTPGenerated && (
         <div>
