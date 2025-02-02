d298b74c0ea7810be6518d1b3718109f5058b321
Hemit Patel <hemitvpatel@gmail.com>
2025-02-01 22:11:51 -0500

    added configs to allow cookies to be sent

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,14 +2,23 @@
 
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const Auth = () => {
+
+  const loginGoogleSSO = async (access_token: string | undefined) => {
+     await axios.post("http://localhost:3001/auth/login", {
+        access_token
+     }, {
+      withCredentials: true // this allows the cookie to be added to the frontend browser
+     })
+  }
   return (
     <div>
       <h2>Login with Google</h2>
       <GoogleLogin
         onSuccess={(response) => {
-          console.log("Access Token:", response.credential); // This is your access token
+          loginGoogleSSO(response.credential); // This is your access token
         }}
         onError={() => {
           alert("An error has occured!")
