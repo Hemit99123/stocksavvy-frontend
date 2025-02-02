648a6b7f1e4e011dbe2a66657c6617cc2eedf984
Hemit Patel <hemitvpatel@gmail.com>
2025-02-02 11:54:19 -0500

    needed env to be NEXT_PUBLIC

--- src/services/httpHeader.ts
+++ src/services/httpHeader.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const httpHeader = axios.create({
-  baseURL: process.env.API_URL, 
+  baseURL: process.env.NEXT_PUBLIC_API_URL, 
   withCredentials: true, // Ensure cookies are sent with each request (for session handling)
 });
 
