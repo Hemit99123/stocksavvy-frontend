6b7d067e3721a1f0ec2d9e0ad6c9d0233288e900
Hemit Patel <hemitvpatel@gmail.com>
2025-02-15 21:26:37 -0500

    added whatsapp + share link

--- src/components/forum/SharePopUp.tsx
+++ src/components/forum/SharePopUp.tsx
@@ -3,7 +3,7 @@
 import useSharePopupStore from "@/store/sharepopup";
 import Link from "next/link";
 import React from "react";
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
 
 
@@ -40,6 +40,7 @@ const SharePopUp = () => {
               { platform: "LinkedIn", icon: <FaLinkedin />, color: "bg-blue-600", shareLink: `https://www.linkedin.com/sharing/share-offsite/?url=${url}` },
               { platform: "X", icon: <FaX />, color: "bg-black" },
               { platform: "Instagram", icon: <FaInstagram />, color: "bg-pink-600" },
+              { platform: "WhatsApp", icon: <FaWhatsapp />, color: "bg-green-600", shareLink: `https://api.whatsapp.com/send?text=Check%20this%20out!%20${url}`}
             ].map(({ platform, icon, color, shareLink }) => (
               <Link
                 href={shareLink || ""}
