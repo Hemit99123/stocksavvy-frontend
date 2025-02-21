b3976cc8dd2ee741ecb28bd762ef646eced9acd0
Hemit Patel <hemitvpatel@gmail.com>
2025-02-20 21:47:11 -0500

    added ?

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,4 +1,5 @@
 import { Hand } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 import { MdHomeFilled } from 'react-icons/md'
 
@@ -6,14 +7,14 @@ const Questions = () => {
   return (
     <div>
         <div className="flex justify-between mx-8 mt-5">
-            <div className="flex justify-center text-green-500">
-              <div className='mt-0.5'>
+            <Link href="/" className="flex justify-center text-green-500 text-lg">
+              <div className='mt-1'>
                 <MdHomeFilled /> 
               </div>
               <div>
                 • StockSavvy
               </div>
-            </div>
+            </Link>
 
             <div className="flex text-gray-600 text-xs gap-2">
               <Hand size={16}/>
@@ -24,7 +25,7 @@ const Questions = () => {
         </div>
 
         <div className="p-8">
-          <h1 className="text-6xl font-bold">Stock Questions</h1>
+          <h1 className="text-6xl font-bold">Stock Questions<span className="text-green-500 font-black">?</span></h1>
           <p className="text-gray-600 text-lg mb-2">Let&apos;s test your financial skills!</p>
         </div>
     </div>
