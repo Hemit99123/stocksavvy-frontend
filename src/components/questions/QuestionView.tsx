ac713858138817885814602d44dcb7dcac279193
Hemit Patel <hemitvpatel@gmail.com>
2025-03-04 21:42:54 -0500

    fully added header

--- src/components/questions/QuestionView.tsx
+++ src/components/questions/QuestionView.tsx
@@ -4,12 +4,24 @@ import { useQuestionTypeStore } from '@/store/questions'
 import GoogleClassroom from "../../../public/icons/classroom.svg"
 import LinkedIn from "../../../public/icons/linkedin.svg"
 import { MdOutlineOpenInNew } from "react-icons/md";
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link';
 
 const QuestionView = () => {
-  const { type } = useQuestionTypeStore() // Use the store's state and setter
 
+  const { type } = useQuestionTypeStore() // Use the store's state and setter
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true) // Set state to true once client-side rendering occurs
+  }, [])
+
+  // Only use window object on the client-side
+  const url = isClient ? encodeURIComponent(window.location.href) : "";
+
+  const googleClassroomShare = `https://classroom.google.com/u/0/share?url=${url}`
+  const linkedInShare = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
+  
   return (
     <div>
       {type === "None" ? (
@@ -23,20 +35,18 @@ const QuestionView = () => {
           <h1 className="text-center text-3xl font-bold mb-2">{type}</h1>
 
           <div className='flex space-x-10'>
-            <div className='cursor-pointer flex items-center space-x-2 text-green-800 text-sm'>
+            <Link href={googleClassroomShare} className='cursor-pointer flex items-center space-x-2 text-green-800 text-sm'>
               <GoogleClassroom />
               <p>Google Classroom</p>
               <MdOutlineOpenInNew />
-            </div>
+            </Link>
 
-            <div className='cursor-pointer flex items-center space-x-2 text-green-800 text-sm'>
+            <Link href={linkedInShare} className='cursor-pointer flex items-center space-x-2 text-green-800 text-sm'>
               <LinkedIn />
               <p>LinkedIn</p>
               <MdOutlineOpenInNew />
-            </div>
+            </Link>
           </div>
-
-
         </div>
       )}
     </div>
