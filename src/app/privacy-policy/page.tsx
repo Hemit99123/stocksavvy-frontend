47a772467ceb114a5fef3f729d47b135daf65065
Hemit Patel <hemitvpatel@gmail.com>
2025-03-16 02:17:57 -0400

    Update page.tsx

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -28,9 +28,7 @@ const PrivacyPolicy = () => {
           {policyContent.map((section) => (
             <div
               key={section.title}
-              className={`bg-white shadow overflow-hidden sm:rounded-lg ${
-                activeSection === section.title ? "border-2 border-green-600" : ""
-              }`}
+              className={"bg-white shadow overflow-hidden sm:rounded-lg"}
             >
               <div className="px-4 py-5 sm:px-6">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">{section.title}</h3>
