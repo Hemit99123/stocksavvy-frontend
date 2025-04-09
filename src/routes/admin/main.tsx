7bcebe331ea487b31ae6c393b80c18a7e566477b
Hemit Patel <hemitvpatel@gmail.com>
2025-04-08 22:05:29 -0400

    updated some more imports

--- src/routes/admin/main.tsx
+++ src/routes/admin/main.tsx
@@ -1,5 +1,5 @@
 import { Grid } from "./components/Dashboard/Info";
-import { Header } from "./components/Dashboard/Header";
+import { Header } from "./components/Dashboard/common/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 
 const Admin = () => {
@@ -7,7 +7,7 @@ const Admin = () => {
     <main className="grid gap-4 p-4 grid-cols-[350px,_1fr]">
       <Sidebar />
       <div className="bg-white rounded-lg pb-4 shadow">
-        <TopBar />
+        <Header />
         <Grid />
       </div>    
     </main>
