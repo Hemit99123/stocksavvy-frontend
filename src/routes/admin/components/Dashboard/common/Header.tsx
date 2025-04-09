cbd0d842f84d8c0f22740586e97d5fd73f9a58c2
Hemit Patel <hemitvpatel@gmail.com>
2025-04-08 22:13:36 -0400

    getting current date

--- src/routes/admin/components/Dashboard/common/Header.tsx
+++ src/routes/admin/components/Dashboard/common/Header.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import { FiCalendar } from "react-icons/fi";
 
 export const Header = () => {
+
+  const dateObj = new Date();
+
+  const monthRef = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  const dateNumber = dateObj.getDate()
+  const month = monthRef[dateObj.getMonth()]
+  const year = dateObj.getFullYear()
+  
+  const date = `${month} ${dateNumber} ${year}`;
+  
   return (
     <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
       <div className="flex items-center justify-between p-0.5">
         <div>
-          <span className="text-sm font-bold block">🚀 Good morning, Tom!</span>
+          <span className="text-sm font-bold block">🚀 Good morning!</span>
           <span className="text-xs block text-stone-500">
-            Tuesday, Aug 8th 2023
+            {date}
           </span>
         </div>
 
