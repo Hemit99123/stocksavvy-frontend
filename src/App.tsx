28ef8886f05dc9c7631c02475c3745763149db1f
Hemit Patel <hemitvpatel@gmail.com>
2025-04-08 21:35:01 -0400

    forgot to remove it from app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Forum from "./routes/forum/main"
 import ID from "./routes/forum/id/main"
 import NotFound from "./routes/not-found"
 import { ToastContainer } from "react-toastify"
-import Admin from "./routes/admin/main"
 
 const App = () => {
   return (
@@ -35,8 +34,7 @@ const App = () => {
         <Route path="privacy-policy" element={<PrivacyPolicy />} />
         <Route path="team" element={<Team />} />
         <Route path="workshop/:slug" element={<WorkShopInfo />} />
-        <Route path="admin" element={<Admin />} />
-        
+
         {/* Not found render */}
         <Route path="*" element={<NotFound />} />
 
