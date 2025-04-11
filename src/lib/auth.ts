1f1f66712aefab021b3700b4d0548c1616d5fbcd
Hemit Patel <hemitvpatel@gmail.com>
2025-04-11 19:16:56 -0400

    no need for a standalone function for toast alerting.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,17 @@
 import httpHeader from '@/services/httpHeader'
 import { toast } from 'react-toastify'
  
-const handleUnauthenticatedUser = async () => {
-        // in case user not logged in (401 error) give them option to log in
-        toast.error('Not authenticated, automatically redirecting...', {
-          onClose: () => {
-            // Redirect after toast disappears
-            window.location.href = "/login"
-          }
-        });
-}
-
 export const handleCheckAuth = async (isAdmin = false) => {
   const apiURL = isAdmin ? "/admin/get-session" : "/auth/get-session"
   const response = await httpHeader.get(apiURL)
 
   if (!response.data.auth) {
-    return handleUnauthenticatedUser()
+    return toast.error('Not authenticated, automatically redirecting...', {
+      onClose: () => {
+        // Redirect after toast disappears
+        window.location.href = "/login"
+      }
+    });
   }
 }
 
