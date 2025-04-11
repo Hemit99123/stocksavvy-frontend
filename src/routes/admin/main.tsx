8300281fd51278a99c15fc6f693adc06d70cc69b
Hemit Patel <hemitvpatel@gmail.com>
2025-04-11 19:43:04 -0400

    added check auth to admin

--- src/routes/admin/main.tsx
+++ src/routes/admin/main.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import { Grid } from "./components/Dashboard/Info";
 import { Header } from "./components/Dashboard/common/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { handleCheckAuth } from "@/lib/auth";
 
 const Admin = () => {
+
+  useEffect(() => {
+    const handleRunCheckAuth = async () => {
+      await handleCheckAuth()
+    }
+
+    handleRunCheckAuth()
+  }, [])
+  
   return (
     <main className="grid gap-4 p-4 grid-cols-[350px,_1fr]">
       <Sidebar />
