34b9c6d75444d0f59fb9fba92292f75a85700176
Hemit Patel <hemitvpatel@gmail.com>
2025-04-11 20:01:24 -0400

    added get session back because it is used (soon navbar will also use it)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
+import httpHeader from '@/services/httpHeader'
 import { toast } from 'react-toastify'
  
+
 export const handleUnauthenticatedUser = () => {
   return toast.error('Not authenticated, automatically redirecting...', {
     onClose: () => {
@@ -7,4 +9,12 @@ export const handleUnauthenticatedUser = () => {
       window.location.href = "/login"
     }
   });
+}
+
+export const handleGetSession = async () => {
+  const response = await httpHeader.get("/auth/get-session")
+
+  return {
+      session: response.data.session,
+  }
 }
